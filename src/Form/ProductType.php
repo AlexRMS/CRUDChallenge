21169f6f9558840bda685b677aff14afe4e35360
alexrms <ricdoa2@gmail.com>
2018-11-19 22:33:45 -0200

    Change views

--- src/Form/ProductType.php
+++ src/Form/ProductType.php
@@ -7,22 +7,19 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class ProductType extends AbstractType
-{
-    public function buildForm(FormBuilderInterface $builder, array $options)
-    {
-        $builder
-            ->add('title')
-            ->add('description')
-            ->add('image')
-            ->add('stock')
-        ;
-    }
+class ProductType extends AbstractType {
+	public function buildForm(FormBuilderInterface $builder, array $options) {
+		$builder
+			->add('title', null, array('label' => 'Título:'))
+			->add('description', null, array('label' => 'Descrição:', 'attr' => array('max-length' => 4000)))
+			->add('image', null, array('label' => 'Url da imagem:'))
+			->add('stock', null, array('label' => 'Quantidade em estoque:', 'attr' => array('min' => 1)))
+		;
+	}
 
-    public function configureOptions(OptionsResolver $resolver)
-    {
-        $resolver->setDefaults([
-            'data_class' => Product::class,
-        ]);
-    }
+	public function configureOptions(OptionsResolver $resolver) {
+		$resolver->setDefaults([
+			'data_class' => Product::class,
+		]);
+	}
 }
