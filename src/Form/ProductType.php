0a3d22184c5c07d3630a425a28094b53f21c351f
alexrms <ricdoa2@gmail.com>
2018-11-20 22:37:55 -0200

    Finish verify

--- src/Form/ProductType.php
+++ src/Form/ProductType.php
@@ -10,7 +10,7 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 class ProductType extends AbstractType {
 	public function buildForm(FormBuilderInterface $builder, array $options) {
 		$builder
-			->add('title', null, array('label' => 'Título:'))
+			->add('title', null, array('label' => 'Título:', 'attr' => array('max-length' => 255)))
 			->add('description', null, array('label' => 'Descrição:', 'attr' => array('max-length' => 4000)))
 			->add('image', null, array('label' => 'Url da imagem:'))
 			->add('stock', null, array('label' => 'Quantidade em estoque:', 'attr' => array('min' => 1)))
