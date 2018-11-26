c468602d3b18f6cbb478ad094f846de89041b9a1
alexrms <ricdoa2@gmail.com>
2018-11-26 20:05:30 -0200

    Finish test

--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -9,83 +9,83 @@ use Doctrine\ORM\Mapping as ORM;
  */
 class Product
 {
-		/**
-		 * @ORM\Id()
-		 * @ORM\GeneratedValue()
-		 * @ORM\Column(type="integer")
-		 */
-		private $id;
+	/**
+	 * @ORM\Id()
+	 * @ORM\GeneratedValue()
+	 * @ORM\Column(type="integer")
+	 */
+	private $id;
 
-		/**
-		 * @ORM\Column(type="string", length=255)
-		 */
-		private $title;
+	/**
+	 * @ORM\Column(type="string", length=255)
+	 */
+	private $title;
 
-		/**
-		 * @ORM\Column(type="text", length=4000, nullable=true)
-		 */
-		private $description;
+	/**
+	 * @ORM\Column(type="text", length=4000, nullable=true)
+	 */
+	private $description;
 
-		/**
-		 * @ORM\Column(type="blob")
-		 */
-		private $image;
+	/**
+	 * @ORM\Column(type="string", length=255)
+	 */
+	private $image;
 
-		/**
-		 * @ORM\Column(type="integer")
-		 */
-		private $stock;
+	/**
+	 * @ORM\Column(type="integer")
+	 */
+	private $stock;
 
-		public function getId(): ?int
-		{
-				return $this->id;
-		}
+	public function getId(): ?int
+	{
+		return $this->id;
+	}
 
-		public function getTitle(): ?string
-		{
-				return $this->title;
-		}
+	public function getTitle(): ?string
+	{
+		return $this->title;
+	}
 
-		public function setTitle(string $title): self
-		{
-				$this->title = $title;
+	public function setTitle(string $title): self
+	{
+		$this->title = $title;
 
-				return $this;
-		}
+		return $this;
+	}
 
-		public function getDescription(): ?string
-		{
-				return $this->description;
-		}
+	public function getDescription(): ?string
+	{
+		return $this->description;
+	}
 
-		public function setDescription(?string $description): self
-		{
-				$this->description = $description;
+	public function setDescription(?string $description): self
+	{
+		$this->description = $description;
 
-				return $this;
-		}
+		return $this;
+	}
 
-		public function getImage()
-		{
-				return $this->image;
-		}
+	public function getImage(): ?string 
+	{
+		return $this->image;
+	}
 
-		public function setImage($image): self
-		{
-				$this->image = $image;
+	public function setImage(?string $image): self
+	{
+		$this->image = $image;
 
-				return $this;
-		}
+		return $this;
+	}
 
-		public function getStock(): ?int
-		{
-				return $this->stock;
-		}
+	public function getStock(): ?int
+	{
+		return $this->stock;
+	}
 
-		public function setStock(int $stock): self
-		{
-				$this->stock = $stock;
+	public function setStock(int $stock): self
+	{
+		$this->stock = $stock;
 
-				return $this;
-		}
+		return $this;
+	}
 }
